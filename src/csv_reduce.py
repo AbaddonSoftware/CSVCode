3541076919dcd4bece83f4349e783d613314a907
AbaddonSoftware <donnie.l.baker@gmail.com>
2024-09-19 10:00:29 -0400

    FIX: first commit of actual code

--- src/csv_reduce.py
+++ src/csv_reduce.py
@@ -11,13 +11,9 @@ def find_line_by_column(reader: csv.reader, column_index, search_term):
             if (row[column_index]).lower() == (search_term).lower():
                 return row
     return None    
-        
-spell_list = ["Alarm (Ritual)", "Light", "Magic Missile", "Vampiric Touch"]
 
-filename = 'Wizard.csv'
-file_obj = open(filename, 'r', newline='')
-reader = open_csv(file_obj)
-# print(find_line_by_column(reader, 1, "Light"))
-for spell in spell_list:
-    print(find_line_by_column(reader, 1, spell))
-file_obj.close()
+with open("Wizard.csv", mode="r", newline="") as file:    
+    test = open_csv(file)
+    print(find_line_by_column(test, 1, "Light"))
+
+
