aee589acff3a57de5b228e17891aa317a94899b6
AbaddonSoftware <donnie.l.baker@gmail.com>
2024-09-19 01:05:56 -0400

    Change: Renamed Project

--- csvreduce.py
+++ csvreduce.py
@@ -1,11 +1,23 @@
 import csv
 
-def open_csv(filename, delimiter):
-    pass
-
-def find_line_by_column(reader, column_index, search_term):
-    pass
+def open_csv(file_obj, delimiter=";"):
+    reader = csv.reader(file_obj, delimiter=delimiter)
+    return reader
 
 
+def find_line_by_column(reader: csv.reader, column_index, search_term):
+    for row in reader:
+        if column_index < len(row):
+            if (row[column_index]).lower() == (search_term).lower():
+                return row
+    return None    
+        
+spell_list = ["Alarm (Ritual)", "Light", "Magic Missile", "Vampiric Touch"]
 
-
+filename = 'Wizard.csv'
+file_obj = open(filename, 'r', newline='')
+reader = open_csv(file_obj)
+# print(find_line_by_column(reader, 1, "Light"))
+for spell in spell_list:
+    print(find_line_by_column(reader, 1, spell))
+file_obj.close()
